4520e00ee93b669c0603a7265973a69a7844b608
Ray <thiagohsegreto@gmail.com>
2026-05-12 21:29:10 -0300

    minor fix: save_internal = 500

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/anymal_d/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/anymal_d/agents/rsl_rl_ppo_cfg.py
@@ -20,7 +20,7 @@ from isaaclab_tasks.manager_based.locomotion.velocity.mdp.symmetry import anymal
 class AnymalDRoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 24
     max_iterations = 1500
-    save_interval = 50
+    save_interval = 500
     experiment_name = "anymal_d_rough"
     obs_groups = {"actor": ["policy"], "critic": ["policy"]}
     actor = RslRlMLPModelCfg(
