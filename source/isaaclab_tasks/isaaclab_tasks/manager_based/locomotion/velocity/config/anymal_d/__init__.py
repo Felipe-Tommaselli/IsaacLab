58b894e67b674e1603dba2031ef1aa898db571e2
Felipe Tommaselli <f.tommaselli@usp.br>
2026-05-14 16:01:57 -0300

    feat: implementing linop variant

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/anymal_d/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/anymal_d/__init__.py
@@ -53,6 +53,18 @@ gym.register(
 )
 
 gym.register(
+    id="Isaac-Velocity-Flat-Anymal-D-LinOp-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:AnymalDFlatEnvCfg",
+        "rsl_rl_cfg_entry_point": (
+            f"{agents.__name__}.rsl_rl_ppo_linop_cfg:AnymalDFlatPPOLinOpRunnerCfg"
+        ),
+    },
+)
+
+gym.register(
     id="Isaac-Velocity-Flat-Anymal-D-Simba-PFO-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
